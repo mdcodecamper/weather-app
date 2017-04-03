5e18769e00ac82f55fef5f9a997031c5408f8559
Saney Alam <codecamper@outlook.com>
2017-04-03 01:31:27 -0400

    Uploaded

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    var api_key = "ef0e314e120c47c48e3152659170204&q";
+    var loc;
+
+    $.getJSON('http://ipinfo.io', function(d){
+        //noinspection JSUnresolvedVariable
+        loc= d.location.split(",");
+
+        var apiLink = "http://api.apixu.com/v1/forecast.json?key=";
+        var days7 ="&days=7";
+        var api = apiLink + api_key + loc[0] + ',' + loc[1]+ days7;
+
+        console.log(api);
+
+        $.getJSON(api, function(v1){
+
+        });
+    });
+
+});
+
+
